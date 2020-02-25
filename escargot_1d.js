30d20f50e1e1727598ab113d9cf4f38162d2722b
Christian Tschacher <ctschacher@axway.com>
2020-02-25 18:14:22 +0100

    added more tests for escargot_1d.js

--- escargot_1d.js
+++ escargot_1d.js
@@ -1,6 +1,10 @@
 // output all elements of a square matrix in clockwise spiral order 
 
 function escargot(orgMatrix, element) { 
+    // check if input is an array
+    if(!Array.isArray(orgMatrix)) 
+        throw new Error("Input is not an array");
+
     if (orgMatrix.length === 0 ) return [];
 
     if (orgMatrix.length % element !== 0) throw new Error("Input array doesn't form a complete matrix with a width of " + element);         // skip edge length that is not a divisor of total number of elements
@@ -19,11 +23,9 @@ function escargot(orgMatrix, element) {
         rowSize-=2; 
         columnSize-=2; 
     }
-
     return result;
 }
 
 exports.escargot = escargot;
 
 // tests for escargot function are performed using test/escargot_1dTest.js (covered by 'npm test' from command-line)
-// escargot(sampleMatrix10);
--- test/escargot_1dTest.js
+++ test/escargot_1dTest.js
@@ -1,11 +1,14 @@
 const should = require('should');
-const matrix = require('../escargot_1d_input3.js');
+const matrix = require('../escargot_1d.js');
 
 var displayMatrix = function(array, width) {
-    console.log(`\n\tInput array (displayed as matrix with ${width} elements per row):`);
-    for(var i = 0; i < array.length; i++) {
-        process.stdout.write(`\t${array[i]}`);
-        if((i+1) % width == 0) console.log();
+    if(!Array.isArray(array)) console.log('\n\tInput: "' + array + '"');
+    else {
+        console.log(`\n\tInput array (displayed as matrix with ${width} elements per row):`);
+        for(var i = 0; i < array.length; i++) {
+            process.stdout.write(`\t${array[i]}`);
+            if((i+1) % width == 0) console.log();
+        }
     }
 }
 
@@ -62,7 +65,7 @@ describe('\n\nCheck 1d-array implementation of escargot algorithm:', function() 
 
     var sampleMatrix3 = [ 1 ];
     it('should return an array with a single element', function() {
-        displayMatrix(sampleMatrix3);
+        displayMatrix(sampleMatrix3, 1);
         var resultArray = matrix.escargot(sampleMatrix3, 1);
         process.stdout.write("\tOutput: " );
         console.log(resultArray);
@@ -80,41 +83,30 @@ describe('\n\nCheck 1d-array implementation of escargot algorithm:', function() 
         should(function() {matrix.escargot(sampleMatrix3, 3)}).throw("Input array doesn't form a complete matrix with a width of " + 3);
     });
 
-    
-    // special characters
-    // non-array
-    // array with 0,1 element
-    // multi-dim array
+    var sampleMatrix4 = [ 'test', 2 , [ 3 ], 1.44];
+    it('should return an array with mixed types of element', function() {
+        displayMatrix(sampleMatrix4, 2);
+        var resultArray = matrix.escargot(sampleMatrix4, 2);
+        process.stdout.write("\tOutput: " );
+        console.log(resultArray);
+        should(resultArray).eql([ 'test', 2, 1.44, [ 3 ]]);
+    });
 
+    var sampleMatrix5 = 'noMatrix';
+    it('should return an array with mixed types of element', function() {
+        displayMatrix(sampleMatrix5, 3);
+        try {
+            var resultArray = matrix.escargot(sampleMatrix5);
+            console.log('\n\tOutput: ' + resultArray);   // in case of thrown error this line is not executed
+        } catch (error) {
+            console.log('\n\tOutput: ' + error);
+        }
+        should(function() {matrix.escargot(sampleMatrix5, 1)}).throw("Input is not an array");
+    });
 });
 
 
-
-// var sampleMatrix1 = [
-//     1,  2,  3,  4,
-//     5,  6,  7,  8, 
-//     9, 10, 11, 12,
-//    13, 14, 15, 16
-// ];
-
-// var sampleMatrix2 = [
-//     1,  2,  3,  4,  5,
-//     6,  7,  8,  9, 10, 
-//    11, 12, 13, 14, 15,
-//    16, 17, 18, 19, 20,
-//    21, 22, 23, 24, 25
-// ];
-
-// var sampleMatrix3 = [
-//     1,  2,  3,  4,  5,  6, 107,
-//     7,  8,  9, 10, 11, 12, 108,
-//    13, 14, 15, 16, 17, 18, 109,
-//    19, 20, 21, 22, 23, 24, 110,
-//    25, 26, 27, 28, 29, 30, 111,
-//    31, 32, 33, 34, 35, 36, 112
-// ];      
-
-// var sampleMatrix4 = [
+// var bigSampleMatrix = [
 //     1,  2,  3,  4,  5,  6,  7,  8,
 //     9, 10, 11, 12, 13, 14, 15, 16,
 //    17, 18, 19, 20, 21, 22, 23, 24, 
