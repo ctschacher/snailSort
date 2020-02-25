0fee24e2532a1236f2346d28e787222f7d085688
Christian Tschacher <ctschacher@axway.com>
2020-02-25 13:00:58 +0100

    Bug fix and new tests added

--- test/escargotTest.js
+++ test/escargotTest.js
@@ -2,11 +2,14 @@ const should = require('should');
 const matrix = require('../escargot.js');
 
 displayMatrix = function(matrix) {
-    console.log("\n\tInput: ");
-    matrix.map((row)=> {
-        process.stdout.write('\t');
-        console.log(row);
-    });
+    if(!Array.isArray(matrix)) console.log('\n\tInput: "' + matrix + '"');
+    else {
+        console.log("\n\tInput: ");
+        matrix.map((row)=> {
+            process.stdout.write('\t');
+            console.log(row);
+        });
+    }
     // matrix.map((row)=> {console.log('\t[ ' + row.join(",\t") + ' ],')}).join("\n");
     process.stdout.write('\tOutput: ');
 }
@@ -35,7 +38,6 @@ describe('Check implementation of escargot algorithm:', function() {
         console.log(resultArray);
         should(resultArray).eql([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
     });
-
 
     sampleMatrix3 = [
         [1, 2, 3, 4, 5, 6, 7, 8],
@@ -76,13 +78,43 @@ describe('Check implementation of escargot algorithm:', function() {
         [25],
         [31]
     ]; 
-    it('should return an array for a single-line (one column) matrix', function() {
+    it('should return an array for a matrix with 1 column', function() {
         displayMatrix(sampleMatrix6);
         var resultArray = matrix.escargot(sampleMatrix6);
         console.log(resultArray);
         should(resultArray).eql([1, 7, 13, 19, 25, 31]);
     });
 
+
+    sampleMatrix6b = [
+        [1, 2], 
+        [7, 8],
+        [13, 14],
+        [19, 20],
+        [25, 26],
+        [31, 32]
+    ]; 
+    it('should return an array for a matrix with 2 columns', function() {
+        displayMatrix(sampleMatrix6b);
+        var resultArray = matrix.escargot(sampleMatrix6b);
+        console.log(resultArray);
+        should(resultArray).eql([1, 2, 8 , 14, 20, 26, 32, 31, 25, 19, 13, 7]);
+    });
+
+    sampleMatrix6c = [
+        [1, 2, 3], 
+        [7, 8, 9],
+        [13, 14, 15],
+        [19, 20, 21],
+        [25, 26, 27],
+        [31, 32, 33]
+    ]; 
+    it('should return an array for a matrix with 3 columns', function() {
+        displayMatrix(sampleMatrix6c);
+        var resultArray = matrix.escargot(sampleMatrix6c);
+        console.log(resultArray);
+        should(resultArray).eql([1, 2, 3, 9, 15, 21, 27, 33, 32, 31, 25, 19, 13, 7, 8, 14, 20, 26]);
+    });
 
     sampleMatrix7 = [
         ['c']
@@ -133,6 +165,23 @@ describe('Check implementation of escargot algorithm:', function() {
     it('should return an error due to mal-formed input matrix (missing second row element)', function() {
         displayMatrix(sampleMatrixB);    
         should(function () {matrix.escargot(sampleMatrixB)}).throw('Input error: Each row should contain the same number of elements to form a proper matrix');
+        console.log('ERROR');
+    });
+
+    var sampleMatrixC = [
+        [ , 2],
+        [7]
+    ]; 
+    it('should return an error due to mal-formed input matrix', function() {
+        displayMatrix(sampleMatrixC);    
+        should(function () {matrix.escargot(sampleMatrixC)}).throw('Input error: Each row should contain the same number of elements to form a proper matrix');
+        console.log('ERROR');
+    });
+
+    var noMatrix = 'noMatrix'; 
+    it('should return an error when input is not an array', function() {
+        displayMatrix(noMatrix);    
+        should(function () {matrix.escargot(noMatrix)}).throw('Input is not an array');
         console.log('ERROR');
     });
 
